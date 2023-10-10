03fb2908ebc4d3cda1a4e5a0283daa1ab8f56e53
dominickasanga <dominickasanga@gmail.com>
2023-10-10 17:39:03 +0200

    modified:   .env.example modified:   celerybeat-schedule

--- toolbox_alerts/tasks.py
+++ toolbox_alerts/tasks.py
@@ -3,35 +3,42 @@ from celery import shared_task
 from decouple import config
 import requests
 
+auth_token = config('AUTH_TOKEN')
+headers = {
+    'Authorization': auth_token
+}
+
 @shared_task
-def findUniqueResourceDetails():
-    # Get the URL and Authorization token from the configuration file
-    url = config('findUniqueResourceDetailsURL')
-    auth_token = config('AUTH_TOKEN')  # Replace with your actual auth token
-
-    print(url)
-    print(auth_token)
-
-    # Create headers with Authorization
-    headers = {
-        'Authorization': auth_token
-    }
+def filterSiteResourceDetailListByZone(data: any):
+    url = config('filterSiteResourceDetailListByZoneURL')
 
     try:
-        # Make the GET request with the headers
         response = requests.get(url, headers=headers)
 
-        # Check the response status code
         if response.status_code == 200:
-            # Successful request, you can process the response data here
             data = response.json()
             print(data)
         else:
-            # Handle non-200 response codes here
+            print(f"Request failed with status code {response.status_code}")
+            return None
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return None
+
+@shared_task
+def findUniqueResourceDetails():
+    url = config('findUniqueResourceDetailsURL')
+
+    try:
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+        else:
             print(f"Request failed with status code {response.status_code}")
             return None
     except Exception as e:
-        # Handle exceptions (e.g., network errors) here
         print(f"An error occurred: {str(e)}")
         return None
 
