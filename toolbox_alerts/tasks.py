9c101256c7bc428a26eef872bd76a80147b8afbc
dominickasanga <dominickasanga@gmail.com>
2023-10-10 23:11:35 +0200

    modified:   .env.example modified:   toolbox_alerts/__pycache__/tasks.cpython-310.pyc modified:   toolbox_alerts/tasks.py

--- toolbox_alerts/tasks.py
+++ toolbox_alerts/tasks.py
@@ -10,33 +10,59 @@ headers = {
 }
 
 @shared_task
+def send_email_notification(data: any):
+    url = config('sendEmailNotificatinForResourceDetailsURL')
+    data = json.loads(data)
+    if data:
+        email_data = {"data": str(data)}
+        try:
+            response = requests.post(url, data=email_data, headers=headers)
+            if response.status_code == 200:
+                response_data = response.json()
+                print(response_data)
+        except Exception as e:
+            print(f"An error occurred in fn1: {str(e)}")
+
+@shared_task
+def findResouceDetailsWithCloseToFullStorage(zone: any):
+    url = config('findResouceDetailsWithCloseToFullStorageURL')
+    zone_data = {"data": str(zone)}  # Assuming 'zone' is the relevant data
+    try:
+        response = requests.post(url, data=zone_data, headers=headers)
+        if response.status_code == 200:
+            response_data = response.json()
+            send_email_notification.delay(response_data)
+    except Exception as e:
+        print(f"An error occurred in fn2: {str(e)}")
+
+@shared_task
+def precursorfindResouceDetailsWithCloseToFullStorage(data: any):
+    data = json.loads(data)
+    for zone in data:
+        findResouceDetailsWithCloseToFullStorage.delay(zone)
+
+@shared_task
 def filterSiteResourceDetailListByZone(data: any):
     url = config('filterSiteResourceDetailListByZoneURL')
-
     try:
         # Define the form data with 'data' as the key and the JSON data as the value
         form_data = data
-
         response = requests.post(url, data=form_data, headers=headers)
-
         if response.status_code == 200:
             response_data = response.json()
-            print(response_data)
-
-        print(f"Request failed with status code {response.status_code}")
-        return None
-
+            precursorfindResouceDetailsWithCloseToFullStorage.delay(response_data)
+        else:
+            print(f"Request failed with status code {response.status_code}")
+            return None
     except Exception as e:
-        print(f"An error occurred: {str(e)}")
+        print(f"An error occurred fn3: {str(e)}")
         return None
 
 @shared_task
 def findUniqueResourceDetails():
     url = config('findUniqueResourceDetailsURL')
-
     try:
         response = requests.get(url, headers=headers)
-
         if response.status_code == 200:
             data = response.json()
             filterSiteResourceDetailListByZone.delay(data)
@@ -44,6 +70,6 @@ def findUniqueResourceDetails():
             print(f"Request failed with status code {response.status_code}")
             return None
     except Exception as e:
-        print(f"An error occurred: {str(e)}")
+        print(f"An error occurred fn4: {str(e)}")
         return None
 
