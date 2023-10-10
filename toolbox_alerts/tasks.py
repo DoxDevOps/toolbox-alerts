ed9465f412966434d06028a3601191d4b920d58b
dominickasanga <dominickasanga@gmail.com>
2023-10-10 18:42:51 +0200

    modified:   toolbox_alerts/__pycache__/tasks.cpython-310.pyc modified:   toolbox_alerts/tasks.py

--- toolbox_alerts/tasks.py
+++ toolbox_alerts/tasks.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from decouple import config
 import requests
+import json
 
 auth_token = config('AUTH_TOKEN')
 headers = {
@@ -13,14 +14,17 @@ def filterSiteResourceDetailListByZone(data: any):
     url = config('filterSiteResourceDetailListByZoneURL')
 
     try:
-        response = requests.get(url, headers=headers)
+        # Define the form data with 'data' as the key and the JSON data as the value
+        form_data = data
+
+        response = requests.post(url, data=form_data, headers=headers)
 
         if response.status_code == 200:
-            data = response.json()
-            print(data)
-        else:
-            print(f"Request failed with status code {response.status_code}")
-            return None
+            response_data = response.json()
+            print(response_data)
+
+        print(f"Request failed with status code {response.status_code}")
+        return None
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
@@ -35,6 +39,7 @@ def findUniqueResourceDetails():
 
         if response.status_code == 200:
             data = response.json()
+            filterSiteResourceDetailListByZone.delay(data)
         else:
             print(f"Request failed with status code {response.status_code}")
             return None
